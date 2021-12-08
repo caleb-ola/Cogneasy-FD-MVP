94687abbf83a8347d23b5e33cabea954a36981cc
Amao Caleb <olajiire2@gmail.com>
2021-12-08 12:53:52 +0100

    after deployment

--- src/components/Reusable-components/mentor_card.js
+++ src/components/Reusable-components/mentor_card.js
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom";
 
 const MentorCard = (props) => {
   return (
-    <div className="card mentor-card border-0 text-start p-3">
+    <div className="card mentor-card border-0 text-start p-3 ">
       <div className="card-body py-3 py-md-4 px-3 px-lg-4 position-relative">
         <div className="">
           <div className="row justify-content-center align-items-center align-content-center my-3">
-            <div className="col-4">
+            <div className="col-12 col-md-4 text-center text-md-left">
               <img
                 src={props.image}
-                className=""
+                className="img-fluid"
                 alt="visual represntation for course"
               />
             </div>
-            <div className="col-8 align-self-center py-3">
+            <div className="col-12 col-md-8 text-center text-md-left align-self-center py-3">
               <h3 className="fs-3 fw-bold lh-1 my-2 mentor-name">
                 {props.name}
               </h3>
